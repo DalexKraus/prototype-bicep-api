6dfa87ce5b63d615054c282fe3e15ef30451aac5
DalexKraus <dalex.kraus@gmail.com>
2021-07-17 18:38:51 +0200

    Initial Commit

---
+++ ExWebAPI/ApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExWebAPI
+{
+    [ApiController]
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        private static readonly string[] Regions =
+        {
+            "Europe",
+            "Asia",
+            "North America",
+            "South America",
+            "Africa",
+            "Australia"
+        };
+        
+        [HttpGet]
+        [Route("regions")]
+        public IActionResult GetRegions()
+        {
+            return Ok(Regions);
+        }
+    }
+}
